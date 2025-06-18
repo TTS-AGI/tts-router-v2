cbcfc1f4fa4b1cbf3929fa289c291ad3595f703b
mrfakename <me@mrfake.name>
2025-06-17 17:30:26 -0700

    Update lanternfish.py

--- tts_providers/lanternfish.py
+++ tts_providers/lanternfish.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import tempfile
+import random
 import httpx
 from loguru import logger
 from typing import Dict, List, Tuple, Any
@@ -56,7 +57,15 @@ class LanternfishProvider(TTSProvider):
                 response = await client.post(
                     os.getenv("LANTERNFISH_API_URL"),
                     json={
-                        "reference_id": os.getenv("LANTERNFISH_REFERENCE_ID"),
+                        "reference_id": random.choice([
+                            '66d8974c34064d529edac2a55079c233',
+                            '761cd2a21135447d9cf827e63d873bd5',
+                            '2eae878771d441668237dffc242ce64f',
+                            '41134c735a9d4a1d895caf908694817b',
+                            '421d581fe0c646019dbb842a44eef8e5',
+                            '55bf518a6b6b485ca88c53caeee5c889',
+                            '57152760c0ad449182c22fcf79edb8f9'
+                        ]),
                         "text": text,
                         "format": "mp3",
                     },
